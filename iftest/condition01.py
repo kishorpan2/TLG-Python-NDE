31f24ec61aa7bd27d1bfa3d88302e47adb9cbd7b
Kishor Pandey <kishor.jpandey@gmail.com>
2022-02-23 18:38:28 +0000

    Updated lab

---
+++ iftest/condition01.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3`
+
+# create the string hostname
+hostname = "MTG"
+# test logic with the `if` statement
+# what to do if this statement is found to be true
+hostname = input("What value should we set for hostname?")
+if hostname.lower() == "mgt":
+    print("The hostname was found to be mtg")
+    print("hostname matches expected config")
+## Always print out to the user
+print("Exiting the script")
+
