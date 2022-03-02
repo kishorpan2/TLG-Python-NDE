ceaa874ee4bb85697dcec9a58ec4635a5ffcbe6e
Kishor Pandey <kishor.jpandey@gmail.com>
2022-03-02 21:23:02 +0000

    :earning how to push data to the screen via debug

--- paramikosftp/sftpmover.py
+++ paramikosftp/sftpmover.py
@@ -8,18 +8,25 @@ import os
 
 ## where to connect to
 t = paramiko.Transport("10.10.2.3", 22) ## IP and port
-
 ## how to connect (see other labs on using id_rsa private/public keypairs)
-t.connect(username="bender", password="alta3")
+#t.connect(username="bender", password="alta3")
+username=input("please enter your username")
+password=input("please enterpassword")
 
 ## Make an sftp connection object
-sftp = paramiko.SFTPClient.from_transport(t)
+if username=="bender" and password =="alta3":
+    t.connect()
+    sftp = paramiko.SFTPClient.from_transport(t)
 
 ## iterate across the files within directory
 for x in os.listdir("/home/student/filestocopy/"): # iterate on directory contents
   if not os.path.isdir("/home/student/filestocopy/"+x): # filter everything that is NOT a directory
     sftp.put("/home/student/filestocopy/"+x, "/tmp/"+x) # move file to target location
-
+try:
+    if(fi) :
+        raise ValueError ('Invalid month')
+except ValueError as msg:
+    print('The program found an ',msg, "error")
 ## close the connections
 sftp.close() # close the connection
 t.close()
